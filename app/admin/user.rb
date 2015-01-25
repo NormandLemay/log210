f88c6218cb89f13e36e08283d85165f987bea235
Normand <lem.norm@gmail.com>
2015-01-25 14:55:32 -0500

    Modification de label

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register User do
   index do
     selectable_column
     id_column
-    column :email
+    column I18n.t("users.email") , :email
     column :current_sign_in_at
     column :sign_in_count
     column :created_at
