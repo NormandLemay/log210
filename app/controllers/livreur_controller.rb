1a0e6c1e35d35b95a4ca47415b49875f54ac3380
K24 <dereck.melancon@gmail.com>
2015-03-30 00:12:24 -0400

    Merge branch 'master' of https://github.com/NormandLemay/log210
    Conflicts:
    	app/controllers/livreur_controller.rb
    	app/views/livreur/index.erb
    	config/routes.rb

--- app/controllers/livreur_controller.rb
+++ app/controllers/livreur_controller.rb
@@ -37,19 +37,19 @@ class LivreurController < ApplicationController
     redirect_to livreur_index_path
   end
 
-  def show
-    @commande = Commande.find_by_id(params[:id])
-  end
+def show
+  @commande = Commande.find_by_id(params[:id])
+end
 
-  private
-  def commande_params
-    params.require(:livreur).permit(:coordonnee, :nom, :prenom)
-  end
+private
+def commande_params
+  params.require(:livreur).permit(:coordonnee, :nom, :prenom)
+end
 
-  def set_livreur
-    if session[:livreur_id].present?
-      @compte_livreur = Livreur.find(session[:livreur_id])
-    end
+def set_livreur
+  if session[:livreur_id].present?
+    @compte_livreur = Livreur.find(session[:livreur_id])
   end
+end
 
 end
