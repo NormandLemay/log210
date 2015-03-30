c3bb806614569ddf18a69a973d84db605143339e
Jimmy Coulombe <Ak93920@ens.etsmtl.ca>
2015-03-29 22:01:38 -0400

    Filtre et correction dans preparer commande

--- app/helpers/restaurateur_helper.rb
+++ app/helpers/restaurateur_helper.rb
@@ -14,7 +14,7 @@ module RestaurateurHelper
    def sortable_heading( label, field, make_link )
      up_params = { :order => field, :direction => 'asc' }
      down_params = { :order => field, :direction => 'desc' }
-     link_to( '^', make_link.call( up_params ) ) + label + link_to( 'v', make_link.call( down_params ) )
+     link_to( '▲', make_link.call( up_params ) ) + label + link_to( '▼', make_link.call( down_params ) )
 
    end
 
